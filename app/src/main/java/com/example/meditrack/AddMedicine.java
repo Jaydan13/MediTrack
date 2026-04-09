ab1c88254055a7109ffcaf460137fba09c7f52a2
jayda <J.Souza1@uni.brighton.ac.uk>
2026-04-09 16:28:13 +0100

    initial project commit

--- app/src/main/java/com/example/meditrack/AddMedicine.java
+++ app/src/main/java/com/example/meditrack/AddMedicine.java
@@ -1,12 +1,19 @@
 package com.example.meditrack;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddMedicine extends AppCompatActivity {
 
@@ -15,6 +22,18 @@ public class AddMedicine extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_medicine);
+
+        EditText medicineNameText = findViewById(R.id.medicationNameText);
+        EditText dosageText = findViewById(R.id.dosageText);
+        EditText intervalText = findViewById(R.id.intervalText);
+        TextView selectedTime = findViewById(R.id.selectedTime);
+        Button timePicker = findViewById(R.id.timePicker);
+        Button saveBtn = findViewById(R.id.saveBtn);
+        Spinner spinnerInterval = findViewById(R.id.spinnerInterval);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("medications");
+
+        String[] options = {"Hours", "Days"};
 
     }
 }
--- app/src/main/java/com/example/meditrack/Login.java
+++ app/src/main/java/com/example/meditrack/Login.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
 
@@ -29,12 +33,21 @@ public class Login extends AppCompatActivity {
         });
 
         Button loginBtn = findViewById(R.id.loginBtn);
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        EditText emailText = findViewById(R.id.emailText);
+        EditText passwordText = findViewById(R.id.passwordText);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        loginBtn.setOnClickListener(view -> {
+            String email = emailText.getText().toString();
+            String password = passwordText.getText().toString();
+
+            auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(a -> {
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(Login.this, HomePage.class);
                 startActivity(intent);
-            }
+            }).addOnFailureListener(e -> {
+                Toast.makeText(this, "Login Unsuccessful", Toast.LENGTH_SHORT).show();
+            });
         });
 
     }
